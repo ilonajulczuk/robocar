2671cb7792249fd8effcc07cb123b42ae6825549
Justyna Ilczuk <att@atte.ro>
2014-06-06 08:48:59 +0200

    Remove unnecessary comments and code.

--- main.c
+++ main.c
@@ -17,20 +17,13 @@ void pwm_init_timer_zero()
     // initialize TCCR0 as per requirement, say as follows
     TCCR0A |= (1<<WGM00)|(1<<COM0A1);
     TCCR0B |= (1<<CS00);
-  
-    // make sure to make OC0 pin (pin PB3 for atmega32) as output pin
-    DDRB |= 0xff;
 }
 
 void pwm_init_timer_two()
 {
     // initialize TCCR2 as per requirement, say as follows
     TCCR2A |= (1<<WGM20)|(1<<COM2A1);
-    
     TCCR2B |= (1<<CS20); 
-    // make sure to make OC0 pin (pin PB3 for atmega32) as output pin
-    DDRB |= (1<<MOTOR_2);
-
 }
 
 void motors_init() {
@@ -67,15 +60,11 @@ void change_spped_of_second_motor(uint8_t change) {
 int main()
 {
     motors_init();
-    duty_motor_1 = 215;       // duty cycle = 45% of 255 = 114.75 = 115
+    duty_motor_1 = 215;
     duty_motor_2 = 215;
-  
-    // initialize timer in PWM mode
     
+    // ride forwards and backwars 
     uint8_t i;
-    // run forever
-
-    
     while(1)
     {
         change_direction();
